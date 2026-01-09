152af059ad6ed2c38b96aafad30b398038552563
abdul rafay <abdulrafaycodeder>
2026-01-09 12:33:47 -0800

    start coding javascript events

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var name  = "John Doe";
+// function popup() {
+//     var popup = document.getElementById("popup").innerText = "kardo app ka naam kya hai mara naa hi pata";
+//     console.log(popup)
+// }
 
-console.log("Hello, " + name + "! Welcome to the Test 2 project.");
+
+function expent() {
+    var expent = document.getElementById("expent").innerText = "kardo app ka naam kya hai mara naa hi pata";
+    console.log(expent)
+}
+
+
+
+function img() {
+    var image = document.getElementById("img").src = "https://mgmotors.com.pk/_next/image?url=%2Fassets%2Foptimized-imgs%2FMG4-crop_11zon.webp&w=1080&q=75"; 
+
+    if (!image === true) {
+        image = document.getElementById("img").src = " https://img.freepik.com/free-psd/red-isolated-car_23-2151852884.jpg?semt=ais_hybrid&w=740&q=80";
+    } 
+
+    
+    console.log(image)
+}
